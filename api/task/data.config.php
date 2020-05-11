6162c04ef5cd2c4adb13fefc2d40fedf76593313
luoxiandong <1741947058@qq.com>
2020-05-11 15:59:52 +0800

    add task config file

---
+++ api/task/data.config.php
@@ -0,0 +1,25 @@
+<?php
+
+$DB_LIST = [
+    //主索引库
+    'index' => [
+        'host'   => '',
+        'port'   => '',
+        'name'   => '',
+        'pwd'    => '',
+        'db'     => '',
+        'slaves' => []  //从库
+    ],
+
+];
+
+$REDIS_LIST = [
+    'index' => [
+        'host' => '',
+        'port' => '',
+        'auth' => '',
+        'dbNumber' => '0',
+    ]
+];
+
+?>
